6271cc6b68f283a1ec0869424fe1208a0d59161a
B.YANG <milanyangbo@hotmail.com>
2019-01-04 14:06:09 +0800

    create config server

---
+++ docloud-config-client/src/main/java/com/docloud/DocloudConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.docloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DocloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocloudConfigClientApplication.class, args);
+	}
+
+}
+
